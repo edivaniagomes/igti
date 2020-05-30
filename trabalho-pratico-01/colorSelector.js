468a8a3d8ad8f29af7085bbbc40a1e35d25c893c
Arthur Penteado <ascpenteado@gmail.com>
2020-05-30 16:08:25 -0300

    Fix

--- trabalho-pratico-01/colorSelector.js
+++ trabalho-pratico-01/colorSelector.js
@@ -1,35 +1,36 @@
-var sample = document.querySelector('#sample');
+window.addEventListener('load', () => {
+    var sample = document.querySelector('#sample');
+});
 
 var newColor = [0, 0, 0];
 
 function changeColor(rangeInput) {
-  var rangeVal = rangeInput.value;
-  var rangeColor = rangeInput.name;
-  switch (rangeColor) {
-    case 'red':
-      newColor[0] = rangeVal;
-      document.querySelector('#red-value').value = rangeVal;
-      changeSample(newColor);
-      break;
+    var rangeVal = rangeInput.value;
+    var rangeColor = rangeInput.name;
+    switch (rangeColor) {
+        case 'red':
+            newColor[0] = rangeVal;
+            document.querySelector('#red-value').value = rangeVal;
+            break;
 
-    case 'green':
-      newColor[1] = rangeVal;
-      document.querySelector('#green-value').value = rangeVal;
-      changeSample(newColor);
-      break;
+        case 'green':
+            newColor[1] = rangeVal;
+            document.querySelector('#green-value').value = rangeVal;
+            break;
 
-    case 'blue':
-      newColor[2] = rangeVal;
-      document.querySelector('#blue-value').value = rangeVal;
-      changeSample(newColor);
-      break;
+        case 'blue':
+            newColor[2] = rangeVal;
+            document.querySelector('#blue-value').value = rangeVal;
+            break;
 
-    default:
-      break;
-  }
+        default:
+            break;
+    }
+
+    changeSample(newColor);
 }
 
 function changeSample(newColor) {
-  sample.style.background =
-    'rgb(' + newColor[0] + ',' + newColor[1] + ',' + newColor[2] + ')';
+    sample.style.background =
+        'rgb(' + newColor[0] + ',' + newColor[1] + ',' + newColor[2] + ')';
 }
